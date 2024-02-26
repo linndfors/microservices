6579f488056ad6047482df45b9ea241ad588b1fa
Olha Nahurna <91615532+linndfors@users.noreply.github.com>
2024-02-26 19:55:52 +0200

    Delete requests_test.py

--- requests_test.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-post_response = requests.post("http://localhost:5000/process", json={"message": "log_msg3"})
-print("POST Response:", post_response.json())
-
-get_response = requests.get("http://localhost:5000/process")
-print("GET Response:", get_response.json())
